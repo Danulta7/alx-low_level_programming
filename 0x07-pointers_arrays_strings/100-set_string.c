eb044a971a44c3870081bb894af35b6855b381ac
Danulta7 <danultaa@gmail.com>
2022-10-31 20:43:14 -0700

    task 0-8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - set valve of pointer to char
+ * @s: the pointer
+ * @to: the char string we to point to
+ *
+ * Return: void
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

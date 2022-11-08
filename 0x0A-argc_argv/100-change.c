d531e104d6e99cbd34bd78d92ddd1026f6e403fb
Danulta7 <danultaa@gmail.com>
2022-11-07 22:09:17 -0800

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,40 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - function
- * @argc: length of argv
- * @argv: number of argument
- * Return: Always 0
+ * maain - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: holds the number of arguements passsed
+ * @argv: the array pointer that holds the arguements passsed
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/
-int position, total, change, aux;
-int coins[] = {15, 10, 5, 2, 1}; /*Array int*/
+	int cents, coins_number = 0;
 
-position = total = change = aux = 0;
-
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-
-total = atoi(argv[1]); /*Covert str to int*/
-
-if (total <= 0)
-{
-printf("0\n");
-return (0);
-}
-
-/*Declaring while*/
-
-while (coins[position] != '\0')
-
-{
-if (total >= coins[position])
-{
-aux = (total / coins[position]);
-change += aux;
-total -= coins[position] * aux;
-}
-
-position++;
-
-}
-
-printf("%d\n", change);
-return (0);
+	if (argc != 2)
+	{
+		printf("Erroe\n")
+		return (1);
+	}
+	else
+	{cents = atoi(argv[1]);
+		while (cents > 0)
+		{
+			if (cents >= 25)
+				cents -= 25;
+			else if (cents >= 10)
+				cents -= 10;
+			else if (cents >= 5)
+				cents -= 5;
+			else if (cents >= 2)
+				cents -= 2;
+			else if (cents >= 1)
+				cents -= 1;
+			coins_number += 1;
+		}
+	}
+	printf("%d\n", coins_number);
+	return (0);
 }
